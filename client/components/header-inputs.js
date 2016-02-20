bcf9f59638b9aa4ca485fe5d18f3adb69b06a5d5
Somebuddy <aleksey.drogovozov@gmail.com>
2016-02-20 16:13:09 +0000

    Clean header input by esc

--- client/components/header-inputs.js
+++ client/components/header-inputs.js
@@ -5,6 +5,16 @@ function resetInput (input, placeholder) {
   input.focus();
 }
 
+function setInputInSearchState(input) {
+  resetInput(input, 'Search website');
+  Session.set('headerInputType', 'search');
+}
+
+function setInputInAddState(input) {
+  resetInput(input, 'Add new website');
+  Session.set('headerInputType', 'add');
+}
+
 Template.header_inputs.onRendered(function (p) {
   Session.set('headerInputType', 'search');
   resetInput(this.find('input'), 'Search website');
@@ -19,20 +29,33 @@ Template.header_inputs.helpers({
 Template.header_inputs.events({
   'click .state.search': function(event, template) {
     if (Session.get('headerInputType') !== 'search') {
-      resetInput(template.find('input'), 'Search website');
-      Session.set('headerInputType', 'search');
+      setInputInSearchState(template.find('input'));
     }
   },
   'click .state.add': function(event, template) {
     if (Session.get('headerInputType') !== 'add') {
-      resetInput(template.find('input'), 'Add new website');
-      Session.set('headerInputType', 'add');
+      setInputInAddState(template.find('input'));
     }
   },
-  'keyup .search-add-inputs.search input': function(event) {
-    console.log('Search input');
+  'keyup .search-add-inputs.search input': function(e, template) {
+    var key = e.which || e.keyCode || 0;
+    if (key === 27) {
+      setInputInSearchState(template.find('input'));
+      return false;
+    } else if (key === 13) {
+      console.log('Search website', template.find('input').value);
+      return false;
+    }
   },
-  'keyup .search-add-inputs.add input': function(event) {
-    console.log('Add input');
+  'keyup .search-add-inputs.add input': function(e, template) {
+    var key = e.which || e.keyCode || 0;
+    if (key === 27) {
+      setInputInAddState(template.find('input'));
+      return false;
+    } else if (key === 13) {
+      console.log('Add website', template.find('input').value);
+      setInputInAddState(template.find('input'));
+      return false;
+    }
   }
 });
