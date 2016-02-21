5db43aafd76a5e4acab5f094ecb0df392ca923e1
Somebuddy <aleksey.drogovozov@gmail.com>
2016-02-21 12:06:55 +0000

    Added momentjs package, added displaying time from weblink create date

--- client/app.js
+++ client/app.js
@@ -1,6 +1,12 @@
+/*global moment, Websites */
+
 /////
 // template helpers 
 /////
+
+Template.registerHelper('timePassed', function(date) {
+  return moment(date).fromNow();
+});
 
 // helper function that returns all available websites
 Template.website_list.helpers({
@@ -8,30 +14,3 @@ Template.website_list.helpers({
     return Websites.find({});
   }
 });
-
-/////
-// template events 
-/////
-
-Template.website_item.events({
-  "click .js-upvote":function(event){
-    // example of how you can access the id for the website in the database
-    // (this is the data context for the template)
-    var website_id = this._id;
-    console.log("Up voting website with id "+website_id);
-    // put the code in here to add a vote to a website!
-
-    return false;// prevent the button from reloading the page
-  }, 
-  "click .js-downvote":function(event){
-
-    // example of how you can access the id for the website in the database
-    // (this is the data context for the template)
-    var website_id = this._id;
-    console.log("Down voting website with id "+website_id);
-
-    // put the code in here to remove a vote from a website!
-
-    return false;// prevent the button from reloading the page
-  }
-})
