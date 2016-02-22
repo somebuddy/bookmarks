aa71c9bb6ecf9a345bc1b0c2b089324d65e96215
Somebuddy <aleksey.drogovozov@gmail.com>
2016-02-22 14:15:01 +0000

    Added details similar websites section

--- client/views/details.js
+++ client/views/details.js
@@ -1,7 +1,19 @@
-/*global history*/
+/*global history, Websites*/
 
 Template.details_header.events({
   'click .close-button': function() {
     history.back();
+  }
+});
+
+Template.recommendations_list.helpers({
+  'sites': function () {
+    if (this.site) {
+      var searchQuery = Meteor.subscribe('searchWebsites', this.site.title + ' ' + this.site.description);
+      var sites = Websites.find({ score: { $exists: true} }, { sort: [['score', 'desc']], limit: 6 });
+      console.log(sites);
+      return sites;
+    }
+    return [];
   }
 })
