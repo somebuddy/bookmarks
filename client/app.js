cd36fe085af7a3d150d8185160f7695029816095
Somebuddy <aleksey.drogovozov@gmail.com>
2016-02-23 08:46:20 +0000

    Open details button moved into separate template

--- client/app.js
+++ client/app.js
@@ -17,29 +17,3 @@ Template.website_list.helpers({
     return Websites.find({}, {sort: {voteUp:-1}});
   }
 });
-
-Template.website_item.events({
-  'click .js-open-details': function(event, template) {
-    var self = this;
-    var el = $('<div class="route-stub"></div>');
-    el.css({left: event.clientX, top: event.clientY});
-    $("body").append(el);
-    el = $("body > .route-stub");
-    el.animate({
-      left: 0,
-      top: 0,
-      opacity: 1,
-      width: '100%',
-      height: '100%',
-    }, 300);
-
-    setTimeout(function () {
-      Router.go('/bookmark/' + self._id);
-      el.animate({
-        opacity: 0,
-      }, 500, function () {
-        el.remove();
-      });
-    }, 300);
-  }
-});
---
+++ client/components/open-details.js
@@ -0,0 +1,37 @@
+/*global Router*/
+
+Template.open_details.events({
+  'click .js-open-details': function(event, template) {
+    var id = this._id;
+    var el = $('<div class="route-stub"></div>');
+    $("body").append(el);
+    $(".page").css({overflow: 'hidden'});
+    el = $("body > .route-stub");
+    
+    // initial state
+    el.css({
+      left: event.clientX, 
+      top: event.clientY,
+      right: $( window ).width() - event.clientX,
+      bottom: $( window ).height() - event.clientY
+    });
+    
+    // final state
+    el.animate({
+      left: "1rem",
+      top: "1rem",
+      opacity: 1,
+      right: "1rem",
+      bottom: "1rem"
+    }, 300);
+
+    setTimeout(function () {
+      Router.go('/bookmark/' + id);
+      el.animate({
+        opacity: 0,
+      }, 500, function () {
+        el.remove();
+      });
+    }, 300);
+  }
+});
