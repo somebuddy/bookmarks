c39ee2ed48a85c699bdcb8cc58749eb119f7ded6
Somebuddy <aleksey.drogovozov@gmail.com>
2016-02-25 11:05:13 +0000

    Excluded current website from recommendation list

--- client/views/details.js
+++ client/views/details.js
@@ -30,7 +30,7 @@ Template.recommendations_list.helpers({
   'sites': function () {
     if (this.site) {
       var searchQuery = Meteor.subscribe('searchWebsites', this.site.title + ' ' + this.site.description);
-      var sites = Websites.find({ score: { $exists: true} }, { sort: [['score', 'desc']], limit: 6 });
+      var sites = Websites.find({ score: { $exists: true}, _id: {$ne: this.site._id} }, { sort: [['score', 'desc']], limit: 6 });
       return sites;
     }
     return [];
