4dd504411a793df1e45c3fe49065f2b5a34e5474
Somebuddy <aleksey.drogovozov@gmail.com>
2016-02-25 09:14:33 +0000

    Locked commenting for anonymous users

--- client/views/details.js
+++ client/views/details.js
@@ -31,7 +31,6 @@ Template.recommendations_list.helpers({
     if (this.site) {
       var searchQuery = Meteor.subscribe('searchWebsites', this.site.title + ' ' + this.site.description);
       var sites = Websites.find({ score: { $exists: true} }, { sort: [['score', 'desc']], limit: 6 });
-      console.log(sites);
       return sites;
     }
     return [];
--- server/lib/comments.js
+++ server/lib/comments.js
@@ -2,12 +2,17 @@
 
 Meteor.methods({
   postComment: function (site, comment) {
+    if (!Meteor.user()) {
+      var userError = new Meteor.Error("not-allowed", "Anonymous user detected", "You must sign in to add comments");
+      throw userError;
+    }
+
     // clean up comment
     comment = comment || '';
     comment = comment.trim();
-    
+
     console.log('Trying to insert comment', site, comment);
-    
+
     // check if still has text
     if (site && comment.length) {
       Comments.insert({
