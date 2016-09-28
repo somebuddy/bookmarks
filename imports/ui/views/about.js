f841d35da7fbbc4a31c067443b87edcbb889196d
Somebuddy <aleksey.drogovozov@gmail.com>
2016-09-28 10:18:21 +0000

    Add about view features helpers and template rendering

--- imports/ui/views/about.js
+++ imports/ui/views/about.js
@@ -1,1 +1,30 @@
+import { Template } from 'meteor/templating';
 import './about.html';
+
+Template.about_content.helpers({
+  productFeatures() {
+    return [{
+      iconClass: 'fa fa-star-o',
+      name: 'Keep interesting websites<br/>in one place.',
+      description: 'Add and save interesting websites.<br/>Import/export bookmarks from browser.',
+      innerFeatures: [{
+        iconClass: 'fa fa-chrome',
+        stateClass: 'current',
+        name: 'Chrome',
+        description: 'Chrome browser extention to add, import, sync bookmarks'
+      }, {
+        iconClass: 'fa fa-firefox',
+        name: 'Firefox',
+        description: 'Firefox browser extention to add, import, sync bookmarks'
+      }, {
+        iconClass: 'fa fa-edge',
+        name: 'Edge',
+        description: 'Edge browser extention to add, import, sync bookmarks'
+      }, {
+        iconClass: 'fa fa-safari',
+        name: 'Safari',
+        description: 'Safari browser extention to add, import, sync bookmarks'
+      }],
+    }];
+  }
+});
