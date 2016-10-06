5791efdf752cd688fe903cdbd52a26ffa0b10886
Somebuddy <aleksey.drogovozov@gmail.com>
2016-10-06 08:03:56 +0000

    Add helpers for current route

--- imports/ui/helpers/helpers.js
+++ imports/ui/helpers/helpers.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import { getWebshotFileName } from '/imports/api/webshots/client.js';
 import timePassed from './time-passed.js';
 import getUserName from './get-user-name.js';
@@ -6,3 +7,15 @@ import getUserName from './get-user-name.js';
 Template.registerHelper('getWebshotFileName', getWebshotFileName);
 Template.registerHelper('timePassed', timePassed);
 Template.registerHelper('getUser', getUserName);
+
+Template.registerHelper('console', o => { console.log(o); });
+
+Template.registerHelper('currentRouteName', () => {
+  FlowRouter.watchPathChange();
+  return FlowRouter.current().route.name;
+});
+
+Template.registerHelper('isCurrentRoute', (name) => {
+  FlowRouter.watchPathChange();
+  return FlowRouter.current().route.name === name;
+});
